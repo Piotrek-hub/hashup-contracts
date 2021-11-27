3b1fe8a368fdcf7ca464748ef40defc1448f68b3
Piotrek <65945270+Piotrek-hub@users.noreply.github.com>
2021-11-27 13:13:29 +0100

    dziala

--- contracts/Comments.sol
+++ contracts/Comments.sol
@@ -1,28 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-<<<<<<< Updated upstream
-/*
-    1. Dodanie msg.sender do listy 
-    2. Przy pierwszym onLoad na strone zapisanie id bloku + sprawdzenie czy w liscie msg.sender zawiera sie adres ktory wykonywał transakcje
-    3.1 Jeśli msg.sender zrobił transakcje to 
-*/
-
-=======
->>>>>>> Stashed changes
 contract Comments {
+
     event commentAdded(
         address author,
         string content,
         uint256 tips,
-        uint256 timestamp
+        uint timestamp
     );
 
-<<<<<<< Updated upstream
-    event commentTipped(uint256 id, uint256 amount);
-
-    event commentUnTipped(uint256 id, uint256 amount);
-=======
     event commentTipped(
         uint id,
         uint amount
@@ -33,44 +20,27 @@ contract Comments {
         uint unTippedAmount,
         address tipper
     );
->>>>>>> Stashed changes
 
     struct Comment {
-        uint256 id;
+        uint id;
         uint256 tips;
         string content;
         address payable author;
         address[] tippers;
     }
 
-<<<<<<< Updated upstream
-    uint256 public commentCount = 0;
-    uint256 public tipsSum;
-=======
     
     uint public commentCount = 0;
->>>>>>> Stashed changes
     address[] public addressesTipped;
-
-    mapping(uint256 => Comment) public comments;
-    mapping(uint256 => mapping(address => bool)) public tippers; // (Comment.id => (tipper => bool))
-    mapping(uint256 => mapping(address => uint256)) public addressToTips; // (Comment.id => (adres => wartosc tipa))
-
-    function getCommentTippers(uint256 _id)
-        public
-        view
-        returns (address[] memory)
-    {
+    
+    mapping(uint => Comment) public comments;
+    mapping(uint => mapping(address => bool)) public tippers; // (Comment.id => (tipper => bool))
+    mapping(uint => mapping(address => uint)) public addressToTips; // (Comment.id => (adres => wartosc tipa))
+    
+    
+    function getCommentTippers(uint _id) public view returns(address[] memory){
         return comments[_id].tippers;
     }
-<<<<<<< Updated upstream
-
-    function setTip(address _tipper) public {
-        for (uint256 i = 0; i < commentCount; i++) {
-            comments[i].tips = 0;
-            for (uint256 j = 0; j < comments[i].tippers.length; j++) {
-                comments[i].tips += _tipper.balance/(1 ether);
-=======
     
     // function setTips(address[] memory _tippers , uint[] memory _balances) public{
     //     for(uint tipperId = 0; tipperId < _tippers.length; tipperId++) {
@@ -90,71 +60,43 @@ contract Comments {
                 comments[i].tips -= addressToTips[i][_tipper] - _balance;
                 addressToTips[i][_tipper] = _balance;
                 emit commentUnTipped(i, comments[i].tips, _tipper);
->>>>>>> Stashed changes
             }
         }
-        // function setTip(address _tipper, uint _balance) public{
-        //     for(uint i = 0; i < commentCount; i++) {
-        //         if(tippers[i][_tipper] && addressToTips[i][_tipper] != _balance) {
-        //             comments[i].tips -= addressToTips[i][_tipper] - _balance;
-        //             addressToTips[i][_tipper] = _balance;
-        //         }
-        //     }
-        // }
     }
-<<<<<<< Updated upstream
-
-    function getTotalTipsSum() public view returns (uint256) {
-        return tipsSum;
-=======
     
     function getAddressToTips(uint _id, address _tipper) public view returns(uint){
         return addressToTips[_id][_tipper];
->>>>>>> Stashed changes
     }
 
     function getTippers() public view returns (address[] memory) {
         return addressesTipped;
     }
-
-    function addTipper(address _tipper) private returns (bool) {
-        for (uint256 i = 0; i < addressesTipped.length; i++) {
-            if (_tipper == addressesTipped[i]) {
+        
+    function addTipper(address _tipper) private returns(bool){
+        for(uint i = 0; i < addressesTipped.length; i++) {
+            if(_tipper == addressesTipped[i]) {
                 return false;
             }
         }
         addressesTipped.push(_tipper);
         return true;
-    }
-
-    function tipComment(uint256 _id, uint256 _amount) public {
+    }    
+        
+    function tipComment(uint _id, uint _amount) public {
         require(msg.sender != address(0), "Sender can't be address 0");
         require(tippers[_id][msg.sender] == false, "User already tipped");
-
+        
         Comment storage _comment = comments[_id];
-
+        
         _comment.tips += _amount;
-<<<<<<< Updated upstream
-        tipsSum += _amount;
-
-=======
         
         
->>>>>>> Stashed changes
         tippers[_id][msg.sender] = true;
         addressToTips[_id][msg.sender] += _amount;
         _comment.tippers.push(msg.sender);
-
+        
+        
         addTipper(msg.sender);
-<<<<<<< Updated upstream
-
-        comments[_id] = _comment;
-        setTip(msg.sender);
-        emit commentTipped(_id, _amount);
-    }
-
-    function addComment(string memory _content, uint256 _value) public {
-=======
         
         // setTips
         
@@ -174,32 +116,22 @@ contract Comments {
     }
 
     function addComment(string memory _content, uint _value) public {
->>>>>>> Stashed changes
         address[] memory tab;
-        comments[commentCount] = Comment(
-            commentCount,
-            0,
-            _content,
-            payable(msg.sender),
-            tab
-        );
+        comments[commentCount] = Comment(commentCount, 0, _content, payable(msg.sender), tab);
         emit commentAdded(msg.sender, _content, 0, block.timestamp);
         tipComment(commentCount, _value);
         commentCount++;
     }
-
-    function getCommentCount() public view returns (uint256) {
+    
+    function getCommentCount() public view returns (uint){
         return commentCount;
     }
+    
 
-    function getCommentById(uint256 _id) public view returns (Comment memory) {
+    function getCommentById(uint _id) public view returns (Comment memory) {
         return comments[_id];
     }
-<<<<<<< Updated upstream
-}
-=======
     
     function pay() payable public {}
     
 }
->>>>>>> Stashed changes
